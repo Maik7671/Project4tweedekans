5193735c8c10bb77ef60fe86c91f8dc50019fd97
Maik7671 <33252055+Maik7671@users.noreply.github.com>
2018-01-18 11:45:24 +0100

    18-1-2017

---
+++ hostingscript.js
@@ -0,0 +1,23 @@
+function Brons_1_Jaar  (){
+	alert("\u20AC59,99")
+}
+
+function Brons_5_Jaar  (){
+	alert("\u20AC299,99")
+}
+
+function Zilver_1_Jaar  (){
+	alert("\u20AC89,99")
+}
+
+function Zilver_5_Jaar  (){
+	alert("\u20AC399,99")
+}
+
+function Goud_1_Jaar  (){
+	alert("\u20AC99,99")
+}
+
+function Goud_5_Jaar  (){
+	alert("\u20AC589,99")
+}
